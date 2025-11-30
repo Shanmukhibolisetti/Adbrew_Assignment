fd6421d773f2aeb16a079a91496246206f4842f1
Shanmukhi Bolisetti <shanmukhi.bolisetti2004@gmail.com>
2025-11-30 16:39:17 +0530

    Adbrew assignment solution

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,21 +1,82 @@
 import './App.css';
-import logo from './logo.svg';
+import { useEffect,useState } from 'react';
 
 
 export function App() {
+  const [task, setTask] = useState("");
+  const [list, setList] = useState([]);
+
+  const fetchTodos = async () => {
+    try {
+      const res = await fetch("http://localhost:8000/todos");
+      const data = await res.json();
+      setList(data);
+    } catch (err) {
+      console.error("Error fetching todos: ", err);
+    }
+  };
+
+  const handleStatus = async (id) => {
+    setList(list.filter((item) => item._id !== id));
+    try {
+      await fetch(`http://localhost:8000/todos/${id}`, {
+        method: "DELETE",
+      });
+    } catch (err) {
+      console.error("Error deleting task: ", err);
+    }
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!task) {
+      alert("Task cannot be empty");
+      return; 
+    }
+    try {
+      fetch("http://localhost:8000/todos/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ task }),
+      });
+      setTask("");
+      fetchTodos();
+    } catch (err) {
+      console.error("Error adding todo: ", err);
+    }
+    console.log("Submitted task: ", task);
+  }
   return (
     <div className="App">
       <div>
         <h1>List of TODOs</h1>
-        <li>Learn Docker</li>
-        <li>Learn React</li>
+        <h3 color='blue'>Check the box to mark it as completed</h3>
+        {list.length === 0 ? <p>No Tasks available</p> :
+          <ul style={{"listStyle": "none"}}>
+            {list.map((item, index) => (
+                <li key={item._id}>
+                  <input type="checkbox"
+                  onChange={() => handleStatus(item._id)}
+                  />
+                  {item.task}
+                </li>
+            ))}
+
+          </ul>
+        }
       </div>
       <div>
         <h1>Create a ToDo</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label for="todo">ToDo: </label>
-            <input type="text" />
+            <input type="text" value={task} onChange={ (e) => setTask(e.target.value)} />
           </div>
           <div style={{"marginTop": "5px"}}>
             <button>Add ToDo!</button>
--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -3,18 +3,39 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import json, logging, os
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 
 mongo_uri = 'mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
 db = MongoClient(mongo_uri)['test_db']
+collection = db['todo_collection']
 
 class TodoListView(APIView):
 
     def get(self, request):
         # Implement this method - return all todo items from db instance above.
-        return Response({}, status=status.HTTP_200_OK)
+        tasks = list(collection.find())
+        for task in tasks:
+            task['_id'] = str(task['_id']) 
+        return Response(tasks, status=status.HTTP_200_OK)
         
     def post(self, request):
         # Implement this method - accept a todo item in a mongo collection, persist it using db instance above.
-        return Response({}, status=status.HTTP_200_OK)
+        task = request.data.get("task", "")
+        if not task:
+            return Response({"error": "Task field is required."}, status=status.HTTP_400_BAD_REQUEST)
+        collection.insert_one({"task": task})
+
+        return Response({"message": "Task added successfully."}, status=status.HTTP_201_CREATED)
+    
+    def delete(self, request, todo_id):
+        try:
+            result = collection.delete_one({"_id": ObjectId(todo_id)})
+            if result.deleted_count == 0:
+                return Response({"error": "Task not found."}, status=404)
+
+            return Response({"message": "Task deleted successfully."}, status=200)
+
+        except:
+            return Response({"error": "Invalid task ID"}, status=400)
 
